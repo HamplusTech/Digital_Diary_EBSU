08e11096726934b1b54d4a73db10bb0775af1d49
HamplusTech <51116379+HamplusTech@users.noreply.github.com>
2021-06-11 15:08:10 +0100

    Add files via upload

---
+++ Digital_Diary/My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Samtech Digital Diary")> 
+<Assembly: AssemblyDescription("Samtech Digital Diary is a project of Uche Samuel N., a Computer Sceintist of Ebonyi State University, Abraka. 2014/2015 Session. For help go to: www.samtechworld.com/help/dd or call: +2348038878629")> 
+<Assembly: AssemblyCompany("Hamplus Tech")> 
+<Assembly: AssemblyProduct("Digital_Diary")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("30cf397c-a0a8-4266-a0ec-694b02410ed6")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("en-GB")> 
